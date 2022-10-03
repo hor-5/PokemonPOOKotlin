4cf633ed15bf6c554a513a3a7f2cd6c68777fbe5
unknown <hc.horaciocapdevila@gmail.com>
2022-10-03 00:25:36 -0300

    set debilidades y efectividades

--- app/src/main/java/com/example/pokemonpoo/Tipo.kt
+++ app/src/main/java/com/example/pokemonpoo/Tipo.kt
@@ -4,25 +4,40 @@ class Tipo(idTipo:Int,nombreTipo: String) {
 
     var idTipo:Int = idTipo
     var nombreTipo:String = nombreTipo
-    //establecemos la efectividad contra x tipos de pokemon según el tipo de pokemon
-    var efectividad:List<String> =
+    //establecemos la efectividad contra x tipos de pokemon según el tipo de pokemon invocando al metodo set
+    var efectividad:List<String> = setEfectividad()
+
+    //establecemos las debilidad contra X tipos de pokemon según el tipo de pokemon llamando al metodo set
+    var debilidad:List<String> = setDebilidad()
+
+
+    private fun setEfectividad():List<String>{
+
+        var aEfectividad : List<String>
+
         when(this.nombreTipo){
-        "agua" -> {listOf("tierra","fuego","roca")}
-        "fuego" -> {listOf("bicho","planta","acero","hielo")}
-        "planta" -> {listOf("tierra","roca","agua")}
-        "electrico" ->{listOf("volador","agua")}
-        "veneno"-> {listOf("planta","hada")}
-        else -> {listOf("")}
+            "agua" -> {aEfectividad = listOf("tierra","fuego","roca")}
+            "fuego" -> {aEfectividad = listOf("bicho","planta","acero","hielo")}
+            "planta" -> {aEfectividad = listOf("tierra","roca","agua")}
+            "electrico" ->{aEfectividad = listOf("volador","agua")}
+            "veneno"-> {aEfectividad = listOf("planta","hada")}
+            else -> {aEfectividad = listOf("")}
+        }
+        return aEfectividad
     }
-    //establecemos las debilidad contra X tipos de pokemon según el tipo de pokemon
-    var debilidad:List<String> =
+
+    private fun setDebilidad():List<String>{
+        var aDebilidades: List<String>
+
         when(this.nombreTipo){
-        "agua" -> {listOf("electrico","planta")}
-        "fuego" -> {listOf("agua","roca","tierra")}
-        "planta" -> {listOf("bicho","fuego","volador","veneno","hielo")}
-        "electrico" ->{listOf("tierra")}
-        "veneno"-> {listOf("psiquico","tierra")}
-        else -> {listOf("")}
+            "agua" -> { aDebilidades = listOf("electrico","planta")}
+            "fuego" -> {aDebilidades = listOf("agua","roca","tierra")}
+            "planta" -> {aDebilidades = listOf("bicho","fuego","volador","veneno","hielo")}
+            "electrico" ->{aDebilidades = listOf("tierra")}
+            "veneno"-> {aDebilidades = listOf("psiquico","tierra")}
+            else -> {aDebilidades = listOf("")}
+        }
+        return aDebilidades
     }
 
     fun esEfectivoContra(nombreTipo:String):Boolean{
